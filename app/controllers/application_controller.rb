d9d3a52865243edeadc3d573dcf558ca83a64349
Michiganx <michigan@ukr.net>
2015-05-24 19:52:29 +0300

    Static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # random comment
+  # random comme
   protect_from_forgery with: :exception
 end
